e868eccd123f7a9a6ec64b37b95fdb57053675c2
Oskar Weinfurtner <oskar.weinfurtner@icloud.com>
2017-10-24 22:13:46 +0200

    bat script automation

---
+++ gui/sonos-box-automation-script.bat
@@ -0,0 +1,4 @@
+@py.exe C:\path\to\your\pythonScript.py %*
+
+// obiger Pfad muss der absolute Pfad des auszuführenden Programmes sein.
+// alle .bat und .py dateien in Ordner wie z.B. C:\Users\YourName\PythonScripts
