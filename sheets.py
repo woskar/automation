037340016150f2eb6ba8c1cca5fe3151dda14bd0
Oskar Weinfurtner <oskar.weinfurtner@icloud.com>
2017-11-06 22:02:44 +0100

    script

--- sheets.py
+++ sheets.py
@@ -3,3 +3,13 @@
 import requests, os, bs4, threading
 os.makedirs('noten', exist_ok=True)
 
+
+import time, os, requests
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+url = ''
+location = ''
+
+browser = webdriver.Firefox()
+browser.get(url)
