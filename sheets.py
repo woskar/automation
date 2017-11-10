c2e536a935ac525bf4cf48fcb0f6c6256b8394db
Oskar Weinfurtner <oskar.weinfurtner@icloud.com>
2017-11-10 22:25:24 +0100

    sheets updated

--- sheets.py
+++ sheets.py
@@ -16,3 +16,9 @@ browser.get(url)
 
 zettels = browser.find_elements_by_partial_link_text('Text')
 print(zettels)
+
+print('Downloading page %s...' % url)
+res = requests.get(url)
+res.raise_for_status()
+print('works checkpoint 1')
+soup = bs4.BeautifulSoup(res.text, "lxml")
